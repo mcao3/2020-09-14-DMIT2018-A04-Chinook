f0901f95167d8ee2beed1c846db7d32a504e8f11
ming.mcsh@hotmail.com <ming.mcsh@hotmail.com>
2020-09-28 18:14:20 -0600

    Created Album Entity

--- ChinookSystem/ENTITIES/Artist.cs
+++ ChinookSystem/ENTITIES/Artist.cs
@@ -38,6 +38,6 @@ namespace ChinookSystem.ENTITIES
         //part of a relationship to table Albums
         //The Artist is the parent part of the relationship
         //this means that ONE artist has MANY albums *****
-        // public virtual ICollection<Album> Albums { get; set; }
+        public virtual ICollection<Album> Albums { get; set; }
     }
 }
